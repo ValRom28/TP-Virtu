a7d4324a9801d67b023112d87ed8c65dcce76603
ValRom <valentin.romanet@etu.univ-orleans.fr>
2025-02-09 20:38:37 +0100

    correction script de build

--- build-image.sh
+++ build-image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Build des images Docker
-docker build -t utilisateurs ./utilisateurs
-docker build -t fortune ./fortune
-docker build -t flask ./flask
-docker build -t clusterswarm ./clusterswarm
-docker build -t apache ./apache
+docker build -t utilisateurs:latest ./utilisateurs
+docker build -t fortune:latest ./fortune
+docker build -t flask:latest ./flask
+docker build -t clusterswarm:latest ./clusterswarm
+docker build -t apache:latest ./apache
